b0132945f59f41f1959ee25d53d1f0896def1422
Cona <chpark0101@gmail.com>
2023-12-05 00:51:59 +0000

    최종

---
+++ js/regist.js
@@ -0,0 +1,12 @@
+function CheckMemberInfo() {
+
+	var id = document.getElementById("id");
+	var password = document.getElementById("password");
+	var name = document.getElementById("name");
+	var gender = document.getElementById("gender");
+    var email = document.getElementById("email");
+    var phone = document.getElementById("phone");
+    var address = document.getElementById("address");
+
+	 document.newMember.submit()
+}
--- js/validation.js
+++ js/validation.js
@@ -55,16 +55,4 @@ function CheckAddProduct() {
 	 document.newProduct.submit()
 }
 
-function CheckMemberInfo() {
-
-	var id = document.getElementById("id");
-	var password = document.getElementById("password");
-	var name = document.getElementById("name");
-	var gender = document.getElementById("gender");
-    var email = document.getElementById("email");
-    var phone = document.getElementById("phone");
-    var address = document.getElementById("address");
-
-	 document.newMember.submit()
-}
 
